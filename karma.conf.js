252812232aafc5fa9130b77a0831dc23970928ab
Shivansh Kumar <shivansh.dbg@gmail.com>
2024-02-20 01:27:49 +0530

    set up karma-teamcity-reporter

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-chrome-launcher'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-teamcity-reporter')
+        ],
+        client: {
+            clearContext: false,
+            captureConsole: true
+        },
+        coverageIstanbulReporter: {
+            reports: ['teamcity', 'html'],
+            fixWebpackSourcePath: true,
+            thresholds: {
+                statements: 80,
+                lines: 80,
+                branches: 80,
+                functions: 80
+            }
+        },
+        singleRun: true,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: ['--headless', '--disable-gpu', '--no-sandbox']
+            }
+        },
+        coverageReporter: {
+            type: 'in-memory'
+        }
+    });
+}
