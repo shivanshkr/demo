545a03326c9cdb3b79c0f956fecfb48283fe9d83
Shivansh Kumar <shivansh.dbg@gmail.com>
2024-02-20 16:11:09 +0530

    test

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadless'],
+        browsers: ['PhantomJS'],
         customLaunchers: {
             ChromeHeadless: {
                 base: 'Chrome',
